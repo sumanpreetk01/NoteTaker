b21cd57d32f5063b4c966719cce778a261beb905
Sumanpreet Kaur <sharankaur@sharans-air.lan>
2023-07-01 08:41:24 -0400

    added get and post routes and id generator

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
-const { readAndAppend } = require('../../01-Activities/22-Stu_Modular-Routing/Unsolved/helpers/fsUtils')
-
-const PORT = 3001
+const fs = require('fs/promises')
+const uuid = require('./helpers/uuid')
+const PORT = process.env.PORT || 3001
 const app = express()
 
 //Middleware
@@ -14,30 +13,77 @@ app.use(express.static('public'));
 
 //GET route for homepage
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/public/index.html'));
+    res.sendFile(path.join(__dirname,'./public/index.html'));
 })
 
 //GET route for notes page
 app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/public/notes.html'))
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
+})
+
+const readNotes = async () => {
+    try {
+        var data = await fs.readFile("./db/db.json", "utf-8")
+        var parsedData = JSON.parse(data)
+        return parsedData     
+    } catch (error) {
+        console.log(error)
+        return "Couldn't read notes!"
+    }
+}
+
+
+const writeNotes = async (newNotes) => {
+    try {
+        await fs.writeFile("./db/db.json", JSON.stringify(newNotes))
+        return true
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+app.get("/api/notes", async (req,res)=> {
+    const notes = await readNotes()
+    res.json(notes)
 })
 
 //POST route for new note
-app.post('/notes',(req,res)=>{
-    const {title,text} = req.body;
-
+app.post('/api/notes',async (req,res)=>{    
     if(req.body){
+        const {title,text} = req.body;
         const newNote = {
             title,
-            text
+            text,
+            id: uuid()
         };
-        readAndAppend(newNote,'./db/db.json');
-        res.json('Note added successfully');
+        var currentNotes = await readNotes()
+        currentNotes.push(newNote)
+        var successfulWrite = await writeNotes(currentNotes)
+        if(successfulWrite){
+            return res.json('Note added successfully');
+        }
+        return res.json("Error in adding note")
     }else{
-        res.error('Error in adding note')
+        res.json('Error in adding note')
     }
 });
+
+//DELETE route to delete note
+app.delete('/api/notes/:id',async (req,res)=>{
+        const noteID = req.params.id
+    var currentNotes = await readNotes()
+    // reasearch the filer method on objects by a key
+    //when you get your new array, write that
+    var successfulWrite = await writeNotes(newNotesArr)
+    if(successfulWrite){
+        return res.json('Note added successfully');
+    }
+    return res.json("Error in adding note")
+})
 
 app.listen(PORT,()=>
 console.log(`App listening at http://localhost:${PORT}`))
 
+
+readNotes()
